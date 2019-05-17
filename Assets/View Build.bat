c0201307c10ada7b822231445c072da0f449ebd3
Moe Baker <moe4baker@gmail.com>
2019-05-17 15:28:00 +0200

    Work

---
+++ Assets/View Build.bat
@@ -0,0 +1,8 @@
+:://Unity launches the batch file at the root of the project folder
+::so we CD to where the batch file is actually located to ensure similar behaviour
+::when launhing from inside Unity and when executing externally
+cd /D "%~dp0"
+
+cd ..
+cd ./Build
+explorer.exe "%cd%\Zombie Party"
